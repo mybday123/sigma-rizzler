3a6e1253bb7ac826686e04acfe3fc1bf74e7e4b5
Gilbert Owen <psychoganko@gmail.com>
2024-11-04 14:34:39 +0700

    feat: added SIGMA to the project

--- sigma_rizzler.c
+++ sigma_rizzler.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int main() {
-    puts("Sigma skibidi");
+void printAuthentication()
+{
+}
+
+void authRegister()
+{
+}
+void printLogo()
+{
+    printf("________  ______  ________  ___        ___       ___\n");
+    printf("__          __    __        ___ __  __ ___     __   __\n");
+    printf("________    __    __   ___  ___  _  _  ___    __ ___ __\n");
+    printf("      __    __    __    __  ___   __   ___   __       __\n");
+    printf("________  ______  ________  ___        ___  __         __\n");
+}
+
+int main()
+{   
+    printLogo();
+        // do{
+
+        // }while();
     return 0;
 }
